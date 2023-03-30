2f20851f611df55c8047305fdf8f684c0c9c89da
DevBack <heltonlima.ti@live.com>
2023-03-29 21:46:45 -0300

    Initial Commit

---
+++ src/test/java/com/devback/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devback.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
